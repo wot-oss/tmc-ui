e3a012e5b30f5f09eb904d174bc6dba8371f7959
Ricardo Manuel Goncalves da Silva <ricardo-manuel.silva@siemens.com>
2025-11-03 16:03:07 +0000

    feat add new dependencies and configuration for CSS
    Signed-off-by: Ricardo Manuel Goncalves da Silva <ricardo-manuel.silva@siemens.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ItemList from './components/ItemList';
+import Layout from './components/Layout';
 
 const App = () => {
   return (
-    <div className="flex max-h-screen w-screen flex-col">
-      <h1 className="p-4 text-xl font-bold">list</h1>
-      <ItemList />
-    </div>
+    <>
+      <div className="min-h-full">
+        <Layout />
+      </div>
+    </>
   );
 };
 
---
+++ src/global.d.ts
@@ -0,0 +1,40 @@
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+type Link = {
+  self: string;
+  content?: string;
+  [key: string]: string | undefined;
+};
+
+type Version = {
+  description: string;
+  digest: string;
+  externalID: string;
+  links: Link;
+  repo: string;
+  timestamp: string;
+  tmID: string;
+  version: {
+    model: string;
+    [key: string]: string | undefined;
+  };
+};
+
+type Item = {
+  links: Link;
+  repo: string;
+  'schema:author': {
+    'schema:name': string;
+    [key: string]: string;
+  };
+  'schema:manufacturer': {
+    'schema:name': string;
+    [key: string]: string;
+  };
+  'schema:mpn': string;
+  tmName: string;
+  versions: Version[];
+};
