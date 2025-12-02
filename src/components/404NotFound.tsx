83e43914b5f0565220c9bc7b91dfb72f2a67c0fb
Ricardo Manuel Goncalves da Silva <ricardo-manuel.silva@siemens.com>
2025-12-02 17:57:33 +0000

    feat add custom style to 404 error page
    Signed-off-by: Ricardo Manuel Goncalves da Silva <ricardo-manuel.silva@siemens.com>

--- src/components/404NotFound.tsx
+++ src/components/404NotFound.tsx
@@ -7,27 +7,27 @@ const FourZeroFourNotFound: React.FC<{ error: string }> = ({ error }) => {
 
   return (
     <>
-      <div className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="grid min-h-dvh place-items-center bg-bgBodyPrimary px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <p className="text-error text-base font-semibold">Error 404</p>
-          <h1 className="text-textValue mt-4 text-balance text-5xl font-semibold tracking-tight sm:text-7xl">
+          <p className="text-base font-semibold text-error">Error 404</p>
+          <h1 className="mt-4 text-balance text-5xl font-semibold tracking-tight text-textValue sm:text-7xl">
             {error}
           </h1>
-          <p className="text-textLabel mt-6 text-pretty text-lg font-medium sm:text-xl/8">
+          <p className="mt-6 text-pretty text-lg font-medium text-textLabel sm:text-xl/8">
             Oops! We couldn’t locate the catalog you requested. Please double-check the URL endpoint
             on Settings or check if the TMC server is running
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
               to="/settings"
-              className="shadow-xs bg-buttonPrimary text-textWhite hover:bg-buttonOnHover focus-visible:outline-buttonFocus rounded-md px-3.5 py-2.5 text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2"
+              className="shadow-xs rounded-md bg-buttonPrimary px-3.5 py-2.5 text-sm font-semibold text-textWhite hover:bg-buttonOnHover focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-buttonFocus"
             >
               Go to Settings
             </Link>
             <button
               type="button"
               onClick={handleReload}
-              className="shadow-xs bg-buttonPrimary text-textWhite hover:bg-buttonOnHover focus-visible:outline-buttonFocus rounded-md px-3.5 py-2.5 text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2"
+              className="shadow-xs rounded-md bg-buttonPrimary px-3.5 py-2.5 text-sm font-semibold text-textWhite hover:bg-buttonOnHover focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-buttonFocus"
             >
               Reload
             </button>
