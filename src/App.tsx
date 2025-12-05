a8c5ef1cacead2f137ca5660436b113bedc9dc0b
Ricardo M G da Silva <rephyrus0877@protonmail.com>
2025-12-05 22:14:29 +0000

    feat replace settings with env variable for base URL
    Signed-off-by: Ricardo M G da Silva <rephyrus0877@protonmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,16 @@ import React from 'react';
 import { createHashRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Layout from './pages/Layout';
-import Settings from './pages/Settings';
 import Details from './pages/Details';
-import { getLocalStorage } from './utils/utils';
-import { SETTINGS_URL_CATALOG } from './utils/constants';
 import FourZeroFourNotFound from './components/404NotFound';
 import { FilterProvider } from './context/FilterContext';
 
 async function inventoryLoader() {
-  const tmcUrl = getLocalStorage(SETTINGS_URL_CATALOG);
-
-  if (!tmcUrl) {
+  if (!__API_BASE__) {
     throw new Response('Catalog URL not configured', { status: 400 });
   }
 
-  const res = await fetch(`${tmcUrl}/inventory`);
+  const res = await fetch(`${__API_BASE__}/inventory`);
 
   if (!res.ok) {
     throw new Response('Failed to fetch inventory', { status: res.status });
@@ -41,10 +36,6 @@ const router = createHashRouter(
           element: <Layout />,
           loader: inventoryLoader,
           errorElement: <FourZeroFourNotFound error={'Catalog not found'} />,
-        },
-        {
-          path: 'settings',
-          element: <Settings />,
         },
         {
           path: 'details/*',
