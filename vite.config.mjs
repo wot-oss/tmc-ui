7f347073b932998874d8827be940acebd9623a72
Ricardo M G da Silva <rephyrus0877@protonmail.com>
2025-12-05 22:12:30 +0000

    feat change configuration to env variables
    Signed-off-by: Ricardo M G da Silva <rephyrus0877@protonmail.com>

---
+++ vite.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const host = process.env.VITE_API_HOST ?? 'localhost';
+  const port = process.env.VITE_API_PORT ?? '8080';
+  const protocol = process.env.VITE_API_PROTOCOL ?? 'http';
+  const API_BASE = `${protocol}://${host}:${port}`;
+
+  return {
+    plugins: [react()],
+    define: {
+      __API_BASE__: JSON.stringify(API_BASE),
+    },
+    // server: {
+    //   proxy: {
+    //     '/api': {
+    //       target: API_BASE,
+    //       changeOrigin: true,
+    //     },
+    //   },
+    // },
+  };
+});
