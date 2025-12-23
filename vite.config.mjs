3977b180b3eefb0e0e1b6b6562b823e6f5700f22
Ricardo MG Silva <rephyrus0877@protonmail.com>
2025-12-23 14:56:44 +0000

    Update vite.config.mjs
    Co-authored-by: Ege Korkan <egekorkan@gmail.com>

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const host = process.env.VITE_API_HOST ?? 'localhost';
-  const port = process.env.VITE_API_PORT ?? '8080';
-  const protocol = process.env.VITE_API_PROTOCOL ?? 'http';
+  const host = env.VITE_API_HOST ?? 'localhost';
+  const port = env.VITE_API_PORT ?? '8080';
+  const protocol = env.VITE_API_PROTOCOL ?? 'http';
   const API_BASE = `${protocol}://${host}:${port}`;
 
   return {
