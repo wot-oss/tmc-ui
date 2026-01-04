47d692ccf5544056aec4cb394d184264c39b034b
Ricardo MG Silva <rephyrus0877@protonmail.com>
2026-01-04 22:58:15 +0000

    Feat add env variable instead of settings (#17)
    * feat add readme.md, revert commit of Dialog
    
    Signed-off-by: Ricardo M G da Silva <rephyrus0877@protonmail.com>
    
    * docs fix readme
    
    Signed-off-by: Ricardo M G da Silva <rephyrus0877@protonmail.com>
    
    * feat change configuration to env variables
    
    Signed-off-by: Ricardo M G da Silva <rephyrus0877@protonmail.com>
    
    * feat replace settings with env variable for base URL
    
    Signed-off-by: Ricardo M G da Silva <rephyrus0877@protonmail.com>
    
    * feat remove settings page, related components and configuration
    
    Signed-off-by: Ricardo M G da Silva <rephyrus0877@protonmail.com>
    
    * chore: delete unused variable, improve ts check
    
    Signed-off-by: Ricardo M G da Silva <rephyrus0877@protonmail.com>
    
    * Update vite.config.mjs
    
    Co-authored-by: Ege Korkan <egekorkan@gmail.com>
    
    * fix update Readme and configuration files to support env variables
    
    ---------
    
    Signed-off-by: Ricardo M G da Silva <rephyrus0877@protonmail.com>
    Co-authored-by: Ege Korkan <egekorkan@gmail.com>

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,3 @@
-export const SETTINGS_URL_CATALOG = 'settings-url-catalog';
-
 export const INVENTORY_ENDPOINT = 'inventory';
 
 export const THING_MODELS_ENDPOINT = 'thing-models';
---
+++ vite.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+ 
+  const host = env.VITE_API_HOST ?? 'localhost';
+  const port = env.VITE_API_PORT ?? '8080';
+  const protocol = env.VITE_API_PROTOCOL ?? 'http';
+  const API_BASE = `${protocol}://${host}:${port}`;
+
+  return {
+    plugins: [react({ include: /\.(mdx|js|jsx|ts|tsx)$/ })],
+    define: {
+      __API_BASE__: JSON.stringify(API_BASE),
+    },
+  };
+});
